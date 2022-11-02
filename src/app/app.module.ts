d21c49c14f002bff9c7cfa4bb6114e6b5bc405b7
Sebastian Alvarez <salvarez@utp.edu.co>
2022-11-01 20:40:03 -0500

    deploy home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
-
 
 
 
@@ -17,7 +17,7 @@ import { HomeModule } from './modules/home/home.module';
     AppRoutingModule,
     HomeModule
   ],
-  providers: [],
+  providers: [{provide:LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
