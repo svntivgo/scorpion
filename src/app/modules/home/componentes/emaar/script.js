aa1a292e0a78b62cce0eadc765dbe8a0dfd0e211
Sebastian Alvarez <salvarez@utp.edu.co>
2022-11-10 16:31:09 -0500

    avance porque

--- src/app/modules/home/componentes/emaar/script.js
+++ src/app/modules/home/componentes/emaar/script.js
@@ -1,42 +1,14 @@
-addEventListener('DOMContentLoaded', ()=>{
-    const contadores = document.querySelectorAll('.contador_cantidad');
-    const velocidad = 1000;
-
-    const animarContadores = () =>{
-        for(const contador of contadores){
-            const actualizar_contador =() =>{
-                let cantidad_maxima = +contador.dataset.cantidadTotal,
-                valor_actual = +contador.innerText,
-                incremento = cantidad_maxima / velocidad;
-                if(valor_actual < cantidad_maxima){
-                    contador.innerText = Math.ceil(valor_actual + incremento)
-                    setTimeout((actualizar_contador), 5);
-                }else{
-                    contador.innerText = cantidad_maxima;
-
-                }
-            }
-            actualizar_contador();
-        }
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
     }
-
-    const mostrarContadores = elementos =>{
-        elementos.forEach(element => {
-            if(element.isIntersecting){
-                   element.target.classList.add('animar');
-                   element.target.classList.remove('ocultar');
-                   setTimeout(animarContadores, 300)
-            }
-        });
-    }
-
-    const observer = new IntersectionObserver(mostrarContadores, {
-        threshold: 0.75 //0 - 1 
-    })
-
-    const elementosHTML = document.querySelectorAll('.contador')
-    elementosHTML.forEach(elementoHTML =>{
-        observer.observe(elementoHTML)
-    })
-    
-})
+  }, duration);
+});
